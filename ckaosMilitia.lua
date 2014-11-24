b02e512e02b94e3c5abf99204ca2648b80f2c742
ckaotik <ckaotik@gmail.com>
2014-11-24 12:34:48 +0100

    Added indicator for number of followers per mission. Added icons for required abilities. Added counts for reward buttons.

--- ckaosMilitia.lua
+++ ckaosMilitia.lua
@@ -1,7 +1,9 @@
 local addonName, addon, _ = ...
 addon = CreateFrame('Frame')
 
-local skipBattleAnimation = true
+local skipBattleAnimation  = true
+local showExtraMissionInfo = true
+local showRewardCounts     = true
 
 -- GLOBALS: _G, C_Garrison, C_Timer, GameTooltip, GarrisonMissionFrame, GarrisonRecruiterFrame, GarrisonLandingPage
 -- GLOBALS: CreateFrame, IsAddOnLoaded, RGBTableToColorCode
@@ -114,6 +116,130 @@ local function UpdateFollowerTabs(frame)
 	end
 end
 
+-- allow to immediately click the reward chest
+local function SkipBattleAnimation(self, missionID, canComplete, success)
+	self.Stage.EncountersFrame.FadeOut:Play()
+	self.animIndex = GarrisonMissionComplete_FindAnimIndexFor(GarrisonMissionComplete_AnimRewards) - 1
+	self.animTimeLeft = 0
+end
+
+-- add extra info to mission list for easier overview
+local function UpdateMissionList()
+	local self     = GarrisonMissionFrame.MissionTab.MissionList
+	local active   = self.showInProgress
+	local missions = active and self.inProgressMissions or self.availableMissions
+
+	local offset  = HybridScrollFrame_GetOffset(self.listScroll)
+	local buttons = self.listScroll.buttons
+	for i = 1, #buttons do
+		local button = buttons[i]
+		local mission = missions[i + offset]
+		if button:IsShown() then
+			if not button.threats then button.threats = {} end
+			if active then
+				-- show success chance
+				local success = C_Garrison.GetRewardChance(mission.missionID)
+				if not button.successChance then
+					button.successChance = button:CreateFontString(nil, nil, 'GameFontHighlightLarge')
+					button.successChance:SetAllPoints(button.MissionType)
+				end
+				button.successChance:SetText(success..'%')
+				button.successChance:Show()
+			elseif button.successChance then
+				button.successChance:Hide()
+			end
+
+			-- show number of followers
+			if not button.followers then
+				button.followers = button:CreateFontString(nil, nil, 'GameFontNormal')
+				button.followers:SetPoint('CENTER', button, 'TOPLEFT', 40, -16)
+			end
+			local icon = '|TInterface\\FriendsFrame\\UI-Toast-FriendOnlineIcon:0:0:0:0:32:32:4:26:4:26|t'
+			button.followers:SetText(string.rep(icon, mission.numFollowers))
+
+			-- show required abilities
+			local _, _, env, envDesc, envIcon, _, _, enemies = C_Garrison.GetMissionInfo(mission.missionID)
+			local numThreats = 1
+			for j = 1, #enemies do
+				for threatID, threat in pairs(enemies[j].mechanics) do
+					local threatButton = button.threats[numThreats]
+					if not threatButton then
+						threatButton = CreateFrame('Frame', nil, button, 'GarrisonMissionMechanicTemplate', numThreats)
+						if numThreats == 1 then
+							threatButton:SetPoint('TOPLEFT', button.Title, 'BOTTOMLEFT', 0, -2)
+						else
+							threatButton:SetPoint('LEFT', button.threats[numThreats-1], 'RIGHT', 8, 0)
+						end
+						button.threats[numThreats] = threatButton
+					end
+					threatButton.info = threat
+					threatButton.Icon:SetTexture(threat.icon)
+					threatButton:Show()
+					numThreats = numThreats + 1
+				end
+			end
+			-- move title text
+			if numThreats > 1 then
+				local anchorFrom, relativeTo, anchorTo, xOffset, yOffset = button.Title:GetPoint()
+				button.Title:SetPoint(anchorFrom, relativeTo, anchorTo, xOffset, yOffset + 10)
+			end
+
+			-- hide unused threat buttons
+			while button.threats[numThreats] do
+				button.threats[numThreats]:Hide()
+				numThreats = numThreats + 1
+			end
+		end
+	end
+end
+
+local function GetPrettyAmount(value)
+	-- mini money formatter: only handles values > 0
+	value = tostring(value)
+	local gold, silver, copper = value:sub(1, -5), value:sub(-4, -3), value:sub(-2, -1)
+	      gold, silver, copper = (gold or 0)*1, (silver or 0)*1, (copper or 0)*1
+	local stringFormat = string.join('',
+		gold > 0 and '%1$s' or '', gold > 0 and '|cffffd700g|r' or '',
+		silver > 0 and '%2$02d' or '', silver > 0 and '|cffc7c7cfs|r' or '',
+		copper > 0 and '%3$02d' or '', copper > 0 and '|cffeda55fc|r' or '')
+	return (stringFormat):format(gold, silver, copper)
+end
+
+local function OnRewardClick(self, btn, up)
+	HandleModifiedItemClick(self.link)
+end
+
+-- show reward counts
+local function UpdateMissionRewards(self, rewards, numRewards)
+	if numRewards <= 0 then return end
+	local index = 1
+	for id, reward in pairs(rewards) do
+		local button = self.Rewards[index]
+		      -- button.link = nil
+		      -- button:SetScript('OnClick', OnRewardClick) -- TODO/FIXME: supposedly does not exist?
+		local quantity = reward.quantity or reward.followerXP
+		if not reward.itemID then
+			if reward.currencyID == 0 then
+				-- gold coin reward
+				quantity = GetPrettyAmount(quantity)
+			else
+				-- garrison resources/apexis shards/followerXP/playerXP
+				quantity = BreakUpLargeNumbers(quantity)
+			end
+		elseif reward.quantity == 1 then
+			-- show item level
+			local _, link, quality, iLevel = GetItemInfo(reward.itemID)
+			-- button.link = link
+			quantity = ITEM_QUALITY_COLORS[quality].hex .. iLevel .. '|r'
+		end
+		if quantity then
+			button.Quantity:Show()
+			button.Quantity:SetText(quantity)
+		end
+		index = index + 1
+	end
+end
+
 addon:SetScript('OnEvent', function(self, event, ...)
 	if self[event] then self[event](self, event, ...) end
 end)
@@ -139,14 +265,16 @@ function addon:GARRISON_FOLLOWER_LIST_UPDATE()
 	end
 end
 
+if skipBattleAnimation then
+	hooksecurefunc('GarrisonMissionComplete_OnMissionCompleteResponse', SkipBattleAnimation)
+end
+if showExtraMissionInfo then
+	hooksecurefunc('GarrisonMissionList_Update', UpdateMissionList)
+	hooksecurefunc(GarrisonMissionFrame.MissionTab.MissionList.listScroll, 'update', UpdateMissionList)
+end
+if showRewardCounts then
+	hooksecurefunc('GarrisonMissionButton_SetRewards', UpdateMissionRewards)
+end
+
 -- initialize on the currently shown frame
 C_Timer.After(0.1, addon.GARRISON_FOLLOWER_LIST_UPDATE)
-
-if skipBattleAnimation then
-	-- allow to immediately click the reward chest
-	hooksecurefunc('GarrisonMissionComplete_OnMissionCompleteResponse', function(self, missionID, canComplete, success)
-		self.Stage.EncountersFrame.FadeOut:Play()
-		self.animIndex = GarrisonMissionComplete_FindAnimIndexFor(GarrisonMissionComplete_AnimRewards) - 1
-		self.animTimeLeft = 0
-	end)
-end
